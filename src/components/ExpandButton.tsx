5cabfbc8ab5fce3995c5fa1a6475aab5d7125701
James Morrissey <jammim19@gmail.com>
2026-01-07 23:44:53 +0000

    Cleaned up the Expand button component

--- src/components/ExpandButton.tsx
+++ src/components/ExpandButton.tsx
@@ -4,9 +4,8 @@ import iconMinus from "../assets/images/icon-minus.svg";
 
 const ExpandButton = () => {
   return (
-    <button className="hover:bg-purple-200 hover:border">
-      {/* // <button className="border-purple-200 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700"> */}
-      <img src={iconExpand} alt="" srcSet={iconExpand} />
+    <button>
+      <img src={iconExpand} alt="Expand accordion" srcSet={iconExpand} />
     </button>
   );
 };
