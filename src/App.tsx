8c1278569c5a9840bb6a25c36ba21ea6a1614eeb
James Morrissey <jammim19@gmail.com>
2026-01-07 23:57:29 +0000

    Added a custom colour from the style guide to tha TW theme

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,8 @@ function App() {
 
   return (
     <>
-      <main className="faq__main container mx-auto px-4 ">
-        <article className="max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl border border-amber-400  rounded overflow-hidden shadow-lg p-4 mx-auto   text-left">
+      <main className="faq__main container mx-auto px-4 bg-s-g-purple-100 ">
+        <article className="max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl bg-white border border-amber-400  rounded overflow-hidden shadow-lg p-4 mx-auto   text-left">
           <FaqHeader />
           <FaqAccordion />
         </article>
