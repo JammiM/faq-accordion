a69049b2e50f9650334e927c7851746ac35c6dc1
James Morrissey <jammim19@gmail.com>
2026-01-14 21:25:11 +0000

    Changed the border colour of the card

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
   return (
     <>
       <main className="faq__main container mx-auto px-4 bg-s-g-purple-100 ">
-        <article className="max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl bg-white border border-amber-400  rounded overflow-hidden shadow-lg p-4 mx-auto   text-left">
+        <article className="max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl bg-white border border-gray-200  rounded overflow-hidden shadow-lg p-4 mx-auto   text-left">
           <FaqHeader />
           <FaqAccordion />
         </article>
