084306e47098af62b3a260958b830cfac4c49f0d
James Morrissey <jammim19@gmail.com>
2026-01-15 12:51:29 +0000

    Refactored the tw classes for faq section

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import FaqAccordion from "./components/FaqAccordion";
 function App() {
   return (
     <>
-      <main className="faq__main container mx-auto px-4 bg-s-g-purple-100 ">
-        <article className="max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl bg-white border border-gray-200  rounded overflow-hidden shadow-lg p-4 mx-auto   text-left">
+      <main className="faq__main">
+        <article className="max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl bg-white border border-gray-200  rounded overflow-hidden shadow-lg text-left">
           <FaqHeader />
           <FaqAccordion />
         </article>
