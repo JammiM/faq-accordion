3df37f2f86e50738ecb54147abbdd2082c1e7dac
James Morrissey <jammim19@gmail.com>
2026-01-15 14:12:55 +0000

    Added the over lapping effect for the faq section

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ function App() {
   return (
     <>
       <main className="faq__main">
-        <article className="max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl bg-white border border-gray-200  rounded overflow-hidden shadow-lg text-left">
+        <div className="bg-amber-300  bg-[url(./assets/images/background-pattern-mobile.svg)] md:bg-[url(./assets/images/background-pattern-desktop.svg)] h-56"></div>
+        <article className=" container mx-auto max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl bg-white border border-gray-200  rounded overflow-hidden shadow-lg text-left mt-[-60px]">
           <FaqHeader />
           <FaqAccordion />
         </article>
