4dce97472cc365158983579e575aa9a06d20d8a5
James Morrissey <jammim19@gmail.com>
2026-01-16 12:30:31 +0000

    Removed the generic background colour

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <main className="faq__main">
-        <div className="bg-amber-300 bg-[url(./assets/images/background-pattern-mobile.svg)] md:bg-[url(./assets/images/background-pattern-desktop.svg)] h-80"></div>
+        <div className="bg-[url(./assets/images/background-pattern-mobile.svg)] md:bg-[url(./assets/images/background-pattern-desktop.svg)] h-80"></div>
         <article className=" container mx-auto max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl bg-white border border-gray-200 rounded-t-2xl overflow-hidden shadow-lg text-left mt-[-150px]">
           <FaqHeader />
           <FaqAccordion />
