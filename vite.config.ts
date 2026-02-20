eb555038052e97c68f4edb7884ac3ba0343b0bdf
James Morrissey <jammim19@gmail.com>
2026-02-20 23:58:23 +0000

    Added 'vite-plugin-svgr' to allow for svg imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import svgrPlugin from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "",
-  plugins: [tailwindcss(), react()],
+  plugins: [tailwindcss(), react(), svgrPlugin()],
 });
