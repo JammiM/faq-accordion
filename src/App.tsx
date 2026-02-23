3e26409821368e2e6e16285b0ad8a667a9535d41
James Morrissey <jammim19@gmail.com>
2026-02-23 14:28:41 +0000

    Removed un-needed  outer div fragment

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,13 @@ import FaqAccordion from './components/FaqAccordion';
 
 function App() {
 	return (
-		<>
-			<main className="faq__main">
-				<div className="h-80 bg-[url(./assets/images/background-pattern-mobile.svg)] md:bg-[url(./assets/images/background-pattern-desktop.svg)]"></div>
-				<article className="container mx-auto mt-[-150px] max-w-sm overflow-hidden rounded-2xl border border-gray-200 bg-white px-4 text-left shadow-lg md:max-w-md lg:max-w-lg xl:max-w-xl">
-					<FaqHeader />
-					<FaqAccordion />
-				</article>
-			</main>
-		</>
+		<main className="faq__main">
+			<div className="h-80 bg-[url(./assets/images/background-pattern-mobile.svg)] md:bg-[url(./assets/images/background-pattern-desktop.svg)]"></div>
+			<article className="container mx-auto mt-[-150px] max-w-sm overflow-hidden rounded-2xl border border-gray-200 bg-white px-4 text-left shadow-lg md:max-w-md lg:max-w-lg xl:max-w-xl">
+				<FaqHeader />
+				<FaqAccordion />
+			</article>
+		</main>
 	);
 }
 
