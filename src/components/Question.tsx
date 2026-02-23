50796f78801f559784586dfae9a2c9cc5dcc8c3b
James Morrissey <jammim19@gmail.com>
2026-02-23 13:09:28 +0000

    Refactored the Question component

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -13,18 +13,20 @@ const Question = ({ question, answer, isActive }: Props) => {
   const [faqAnswer, showAnswer] = useState(isActive);
 
   return (
-    <div className="mb-2">
-      <hr />{" "}
+    <div className="mb-2 single-question border-b-[1px] border-b-faq-purple-100 pb-6 pt-2">
       <div className="flex justify-between items-center">
         <h2 className="text-1xl font-bold text-gray-900 hover:text-faq-highlight hover:cursor-pointer">
           {question}
         </h2>
 
-        <button onClick={() => showAnswer(!faqAnswer)}>
+        <button
+          onClick={() => showAnswer(!faqAnswer)}
+          aria-label={faqAnswer ? "Hide Answer" : "Display Answer"}
+        >
           {faqAnswer ? <IconMinus /> : <IconExpand />}
         </button>
       </div>
-      {faqAnswer ? <p className="text-gray-800">{answer}</p> : ""}
+      {faqAnswer ? <p className="text-faq-purple-600 pt-4">{answer}</p> : ""}
     </div>
   );
 };
